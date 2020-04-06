585efbf6a2c3923805764d50f1dae55da81fcd77
Saharat Paynok <saharat.paynok@gmail.com>
2020-04-06 15:52:04 +0700

    Demo creating and saving java object to database

---
+++ src/com/chivas/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,42 @@
+package com.chivas.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.chivas.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		// use the session object to save java object
+		try {
+
+			// Create student object
+			System.out.println("Createing new student object");
+			Student student = new Student("Saharat", "Paynok", "saharat.paynok@gmail.com");
+
+			// begin transaction
+			System.out.println("Starting transaction");
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the student object");
+			session.save(student);
+
+			// commit transaction
+			System.out.println("Commit to the database");
+			session.getTransaction().commit();
+
+		} finally {
+			factory.close();
+		}
+
+	}
+}
