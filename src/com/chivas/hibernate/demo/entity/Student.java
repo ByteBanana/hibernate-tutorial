41268ae36f51c709d78d1a259da8c9475f9e1f56
Saharat Paynok <saharat.paynok@gmail.com>
2020-04-06 15:53:01 +0700

    Change column name& add generate strategy

--- src/com/chivas/hibernate/demo/entity/Student.java
+++ src/com/chivas/hibernate/demo/entity/Student.java
@@ -2,6 +2,8 @@ package com.chivas.hibernate.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,13 +12,14 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 
-	@Column(name = "firstName")
+	@Column(name = "first_name")
 	private String firstName;
 
-	@Column(name = "lastName")
+	@Column(name = "last_name")
 	private String lastName;
 
 	@Column(name = "email")
